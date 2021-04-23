c7810c4a640123cddd4340ee7e2189b4d84a6a07
jkjan <jkjan4@gmail.com>
2021-04-24 03:26:56 +0900

    added: cuda info at start

--- src/hyperparameters.py
+++ src/hyperparameters.py
@@ -7,6 +7,20 @@ import sys
 if torch.cuda.is_available():
     sys.stdout.write("CUDA %s is available.\n\n" % torch.version.cuda)
     device = 'cuda'
+    torch.backends.cudnn.benchmark = True
+    cur_device = torch.cuda.current_device()
+    print('Device:      ')
+    print("    Index:   ", cur_device)
+    print("    Name:    ", torch.cuda.get_device_name(cur_device))
+    print('METADATA:    ')
+    print('    CUDA ver: ', torch.version.cuda)
+    print('    Torch ver:', torch.__version__)
+    print('    cuDNN ver:', torch.backends.cudnn.version())
+    print('Memory Usage:')
+    print('    Max Alloc:', round(torch.cuda.max_memory_allocated(cur_device) / 1024 ** 3, 1), 'GB')
+    print('    Allocated:', round(torch.cuda.memory_allocated(cur_device) / 1024 ** 3, 1), 'GB')
+    print('    Cached:   ', round(torch.cuda.memory_reserved(cur_device) / 1024 ** 3, 1), 'GB')
+    print()
 else:
     device = 'cpu'
 
